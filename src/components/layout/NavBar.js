f3ca9cd747cf394ed69ca84dff6704096a6de1c6
EveEB <104207900+EveEB@users.noreply.github.com>
2022-08-09 15:25:29 +0100

    update navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
       <nav>
         <ul className="nav-bar-list">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/tearooms-app/">Home</Link>
           </li>
           <li>
             <Link to="/OurStory">Our Story</Link>
@@ -19,7 +19,7 @@ function NavBar() {
             <Link to="/contact">Contact Us</Link>
           </li>
           <li>
-            <Link to="/MeetTheTeam">Meet the Team</Link>
+            <Link to="/tearooms-app/MeetTheTeam">Meet the Team</Link>
           </li>
           <li>
             <Link to="/Book">Book a Table</Link>
